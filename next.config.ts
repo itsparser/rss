cd5ff56ddd2f4fe21f1a2a3f24f003c4bf9faf15
Vasanth kumar Kalaiselvan <itsparser@gmail.com>
2024-11-07 10:58:41 +0530

    allow global endpoints

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+        ],
+      },
+    ];
+  },
   logging: {
     fetches: {
       hmrRefreshes: true,
